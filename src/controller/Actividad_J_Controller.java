7fc57b28d5dfb8d7f58468eb3c2c3a86b6b15297
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-11-01 18:42:28 +0100

    ADD-crear fxml
    creada la estructura de fxml

--- src/controller/Actividad_J_Controller.java
+++ src/controller/Actividad_J_Controller.java
@@ -1,5 +1,36 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Actividad_J_Controller {
+
+    @FXML
+    private ImageView imgAzulClaro;
+
+    @FXML
+    private ImageView imgAzulOscuro;
+
+    @FXML
+    private ImageView imgBlanco;
+
+    @FXML
+    private ImageView imgGris;
+
+    @FXML
+    private ImageView imgGrisOscuro;
+
+    @FXML
+    private ImageView imgLuz;
+
+    @FXML
+    private ImageView imgMarron;
+
+    @FXML
+    private ImageView imgNegro;
+
+    @FXML
+    private ImageView imgRojo;
+
 
 }
